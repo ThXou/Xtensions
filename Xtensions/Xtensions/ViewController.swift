cdda4f737d9be6cdc60051c5c1481e740e0fc5d1
Luis Cardenas <lcardenas@doonamis.es>
2018-02-01 10:59:53 +0100

    Minor fixes

--- Xtensions/Xtensions/ViewController.swift
+++ Xtensions/Xtensions/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if UIDevice.current.x_isIPad {
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
